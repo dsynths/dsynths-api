039ccced3f0caabe9b9138856d4e8d15afcdcb99
0xgsdc <103629653+0xgsdc@users.noreply.github.com>
2022-04-14 17:51:17 +1000

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,47 @@
-import express from 'express'
-import cors from 'cors'
-import dotenv from 'dotenv'
-import helmet from 'helmet'
+  import express from 'express'
+  import cors from 'cors'
+  import dotenv from 'dotenv'
+  import helmet from 'helmet'
+  import compression from 'compression';
 
-import { cryptoRouter } from './crypto/router'
-import { stocksRouter } from './stocks/router'
-import { errorHandler } from './middleware/error'
-import { notFoundHandler } from './middleware/not-found'
-import { CORS_WHITELIST } from './config'
+  import { cryptoRouter } from './crypto/router'
+  import { stocksRouter } from './stocks/router'
+  import { errorHandler } from './middleware/error'
+  import { notFoundHandler } from './middleware/not-found'
+  import { CORS_WHITELIST } from './config'
 
-dotenv.config()
+  dotenv.config()
 
-if (!process.env.PORT) {
-  console.error('PORT must be a defined environment variable')
-  process.exit(1)
-}
+  if (!process.env.PORT) {
+    console.error('PORT must be a defined environment variable')
+    process.exit(1)
+  }
 
-const PORT: number = parseInt(process.env.PORT as string, 10)
-const app = express()
-app.listen(PORT, () => console.log(`Running on port ${PORT}`))
+  const PORT: number = parseInt(process.env.PORT as string, 10)
+  const app = express()
+  app.listen(PORT, () => console.log(`Running on port ${PORT}`))
+  if (process.env.COMPRESSION) {
+  app.use(compression())
+  }
+  app.use(helmet())
 
-app.use(helmet())
+  app.use(
+    cors({
+      origin: function (origin, callback) {
+        if (!origin || CORS_WHITELIST.indexOf(origin) !== -1) {
+          callback(null, true)
+        } else {
+          callback(new Error('Not allowed by CORS'))
+        }
+      },
+      optionsSuccessStatus: 200, // some legacy browsers (IE11) choke on 204
+      methods: ['GET'],
+    })
+  )
 
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin || CORS_WHITELIST.indexOf(origin) !== -1) {
-        callback(null, true)
-      } else {
-        callback(new Error('Not allowed by CORS'))
-      }
-    },
-    optionsSuccessStatus: 200, // some legacy browsers (IE11) choke on 204
-    methods: ['GET'],
-  })
-)
+  app.use(express.json())
 
-app.use(express.json())
-
-app.use('/stocks', stocksRouter)
-app.use('/crypto', cryptoRouter)
-app.use(errorHandler)
-app.use(notFoundHandler)
+  app.use('/stocks', stocksRouter)
+  app.use('/crypto', cryptoRouter)
+  app.use(errorHandler)
+  app.use(notFoundHandler)
